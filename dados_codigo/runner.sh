52cf0fd28720b8f5ed9b6749f34ea6a1d0894fce
Saint-Clair Lima <meuimeio.com>
2024-06-06 18:27:45 -0300

    Altera endereço de envio para utilizar o proxy que encaminhará ao ORION

--- dados_codigo/runner.sh
+++ dados_codigo/runner.sh
@@ -1,3 +1,4 @@
 cd /home/ubuntu/ProjetoProgDistribuida/dados_codigo/
 # python3 casos_covid.py
-python3 casos_covid.py http://10.3.225.203:1026/v2/entities
+# python3 casos_covid.py http://10.3.225.203:1026/v2/entities
+python3 casos_covid.pyhttp://10.7.99.170:3000/v2/entities
