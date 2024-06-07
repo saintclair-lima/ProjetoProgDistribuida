8dadcf4126ecc3f5daff42ea9a542533f4a641ef
Saint-Clair Lima <meuimeio.com>
2024-06-07 13:01:41 -0300

    Atualização do token

--- dados_codigo/runner.sh
+++ dados_codigo/runner.sh
@@ -1,4 +1,4 @@
 cd /home/ubuntu/ProjetoProgDistribuida/dados_codigo/
 # python3 casos_covid.py
-# python3 casos_covid.py http://10.3.225.203:1026/v2/entities a916b36ac4c1cc82d0e4686a78db6fcda2107f6d
-python3 casos_covid.py http://10.7.99.170:3010/v2/entities a916b36ac4c1cc82d0e4686a78db6fcda2107f6d
+# python3 casos_covid.py http://10.3.225.203:1026/v2/entities 9cca152d3c1241e312e563efa55f0774f7fd637f
+python3 casos_covid.py http://10.7.99.170:3010/v2/entities 9cca152d3c1241e312e563efa55f0774f7fd637f
